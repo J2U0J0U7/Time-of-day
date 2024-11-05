723da5bdbf6bed6fb8c98c9df5765ebd1c21ef29
Julian Isaacs <145123662+J2U0J0U7@users.noreply.github.com>
2024-11-05 12:36:33 -0500

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,23 +1,23 @@
-
 const d = new Date();
-
-let hour =  d.getHours();
+let hour = d.getHours();
 let greetingText = document.getElementById("greeting-text");
 
 if (hour < 12) {
-  greetingText.textContent = "Good Morning!";
-  document.querySelector(".night").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
+    greetingText.textContent = "Good Morning!";
+    document.querySelector(".afternoon").classList.add("d-none");
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".morning").classList.remove("d-none");
 } else if (hour < 17) {
-  greetingText.textContent = "Good Afternoon!";
-  document.getElementsByTagName("body")[0].classList.add("noon-bg");
-  greetingText.parentElement.classList.add("noon-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".night").classList.add("d-none");
-} 
-else {
-  greetingText.textContent = "Good Night!";
-  greetingText.parentElement.classList.add("night-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
+    greetingText.textContent = "Good Afternoon!";
+    document.querySelector("body").classList.add("noon-bg");
+    greetingText.parentElement.classList.add("noon-gradient");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".afternoon").classList.remove("d-none");
+} else {
+    greetingText.textContent = "Good Night!";
+    greetingText.parentElement.classList.add("night-gradient");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".afternoon").classList.add("d-none");
+    document.querySelector(".night").classList.remove("d-none");
 }
